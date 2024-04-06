9b44863e4ee78ba8a28d00b17c2e2b38d4f8c88b
Lucas Muniz <lucaslima1048@gmail.com>
2024-04-06 11:53:48 -0300

    Create project and bd drivers

---
+++ src/main/java/lucas/com/passin/PassInApplication.java
@@ -0,0 +1,12 @@
+package lucas.com.passin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassInApplication.class, args);
+	}
+}
