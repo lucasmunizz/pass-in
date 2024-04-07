ae4f72cef28682da2541409210e5c5e51e1231d9
Lucas Muniz <lucaslima1048@gmail.com>
2024-04-07 00:43:59 -0300

    create endpoints of badge

--- src/main/java/lucas/com/passin/controllers/AttendeeController.java
+++ src/main/java/lucas/com/passin/controllers/AttendeeController.java
@@ -1,18 +1,25 @@
 package lucas.com.passin.controllers;
 
+import lombok.RequiredArgsConstructor;
+import lucas.com.passin.dto.attendees.AttendeeBadgeResponseDTO;
+import lucas.com.passin.services.AttendeeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/attendees")
+@RequiredArgsConstructor
 public class AttendeeController {
 
+    private final AttendeeService attendeeService;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<String> getEvent(@PathVariable String id){
-        return ResponseEntity.ok("sucesso!");
+    @GetMapping("/{attendeeId}/badge")
+    public ResponseEntity<AttendeeBadgeResponseDTO> getAttendeeBadge(@PathVariable String attendeeId, UriComponentsBuilder uriComponentsBuilder){
+        AttendeeBadgeResponseDTO responseDTO = this.attendeeService.getAttendeeBadge(attendeeId, uriComponentsBuilder);
+        return ResponseEntity.ok(responseDTO);
     }
 }
